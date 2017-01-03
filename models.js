d581234947a10841a41c45862634303a2405a39c
steviebuckets <steviebuckets33@gmail.com>
2017-01-03 15:06:15 -0800

    multiple changes

---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Upload = require('s3-uploader');
+
+//this is the structure for my blog posts
+const blogPostsSchema = mongoose.Schema({
+	"title": {type: String, required: true},
+	"recordstore": {type: String, required: true},
+	"description": {type: String, required: true},
+	"user": {type: String, required: true},
+	"created": {type: Date, default: Date.now}
+	//"photo": {type: img, required: true}
+
+});
+
+blogPostsSchema.methods.apiRepr = function() {
+	return {
+		id: this._id,
+		title: this.title,
+		recordstore: this.record_store,
+		description: this.description,
+		user: this.user,
+		created: this.created
+    	//photo
+
+	};
+}
+
+const BlogPost = mongoose.model('BlogPost', blogPostsSchema);
+
+module.exports = {BlogPost};
--- server.js
+++ server.js
@@ -1,4 +1,155 @@
 var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Upload = require('s3-uploader');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {BlogPost} = require('./models');
+
+const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.use(bodyParser.json());
+
+//get posts
+app.get('/posts', (req, res) => {
+    BlogPost
+        .find()
+        .limit(12)
+        .exec()
+        .then(posts => {
+            res.json({
+                posts: posts.map(
+                    (post) => post.apiRepr())
+            });
+            console.log("working");
+        })
+        .catch(
+            err => {
+                console.error(err);
+                res.status(500).json({ message: 'Internal sever error' });
+            });
+});
+
+app.get('/posts/:id', (req, res) => {
+    BlogPost
+        .findById(req.params.id)
+        .exec()
+        .then(post => res.json(post.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went horribly awry' });
+        });
+});
+
+//posts
+app.post('/posts', (req, res) => {
+    const requiredFields = ['title', 'recordstore', 'description', 'author' /*photo*/];
+    requiredFields.forEach(field => {
+        if (!(field in req.body && req.body[field])) {
+            return res.status(400).json({ message: `Must specify value for ${field}` });
+        }
+    });
+    BlogPost
+        .create({
+            title: req.body.title,
+            recordstore: req.body.recordstore,
+            description: req.body.description,
+            author: req.body.author
+            //photo:
+        })
+        .then(
+            blogPost => res.status(201).json(blogPost.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        });
+});
+
+//put/update
+app.put('/posts/:id', (req, res) => {
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id ` +
+            `(${req.body.id}) must match`);
+        console.error(message);
+        res.status(400).json({ message: message });
+    }
+
+    const toUpdate = {};
+    const updateableFields = ['title', 'description', 'recordstore'];
+
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            toUpdate[field] = req.body[field];
+        }
+    });
+
+    BlogPost
+        .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+        .exec()
+        .then(updatedPost => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+
+//delete posts
+app.delete('/posts/:id', (req, res) => {
+    BlogPosts
+        .findByIdAndRemove(req.params.id)
+        .exec()
+        .then(blogPost => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+//catch-all endpoint if client makes request to non existent endpoint
+app.use('*', function(req, res) {
+    res.status(404).json({ message: 'Not Found' });
+});
+
+//close server access object, only created when we runServer
+let server;
+
+//this function connects to our databse, then starts server
+function runServer() {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(DATABASE_URL, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(PORT, () => {
+                    console.log(`Your app is listening on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+//this function closes server, and returns a promise
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+
+}
+
+
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+module.exports = { app, runServer, closeServer };
