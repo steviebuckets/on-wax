1da3f6e3bfb182428587b63af1611cc7feaa185d
steviebuckets <steviebuckets33@gmail.com>
2017-01-09 20:09:58 -0800

    major overhaul, went back to bootstrap, posts are now working and added dynamically to working grid

--- public/js/app.js
+++ public/js/app.js
@@ -2,55 +2,29 @@
 $(function() {
     $.getJSON('/posts', function(data) {
 
-        var $grid = $('.grid').masonry({
-
-            itemSelector: '.grid-item',
-            columnWidth: 160
-
-        });
-
+        
 
         $.each(data.posts, function(i, data) {
             var div_data =
-                "<div>" + data.title + "<br/>" + data.recordstore + "<br/>" + data.description + "<br/>" + data.user + "<br/> <p class='date'>" + data.created + "</div>";
+                "<div>" + '<p class="title">' + data.title + "</p><br/>" + data.recordstore + "<br/>" + data.description + "<br/>" + data.user + "<br/> <p class='date'>" + data.created + "</div>";
 
-            var $items = $('<div class="grid-item"></div>');
-            ($items).append(div_data);
-            $('.grid').prepend($items);
-
-
+           var $items = $('<div class="col-md-4"></div');
+            $items.append(div_data)
             
-
-            // add and lay out newly prepended items
-            $grid.masonry('reloadItems')
-            .masonry('prepended', $items);
+            $('.row').append($items);
+                
 
         });
 
 
-        // init Isotope
-        var $grid = $('.grid').isotope({
-
-            getSortData: {
-                created: '[.date]',
-                dated: function(itemElem) {
-                        var dated = $(itemElem).find('.date');
-
-                    } // text from querySelector
-
-            }
-        });
-
-
-        //search with isotope by class in my html to locate the date and filter by last added
-        $grid.isotope({ sortAscending: false, dated: true });
+      
 
 
     });
 
 
     //this creates a new post on submit from user-posts form.
-    $("#user-posts").submit(function(event) {
+    $('.form-group').submit(function(event) {
         event.preventDefault();
         var data = {
             user: $('#user').val(),
@@ -60,7 +34,7 @@ $(function() {
         }
 
         addData(data);
-        $("#user-posts")[0].reset();
+        $('.form-group')[0].reset();
 
     });
 
@@ -77,9 +51,9 @@ $(function() {
                 console.log(responseData);
 
                 var div_data =
-                    '<div class="grid-item" style="position: relative; height: 132px;">' + responseData.title + "<br/> " + responseData.recordstore + "<br/>" + responseData.description + "<br/>" + responseData.user + "<br/>" + responseData.created + "</div>";
+                    '<div class="col-6 col-md-4">' + responseData.title + "<br/> " + responseData.recordstore + "<br/>" + responseData.description + "<br/>" + responseData.user + "<br/>" + responseData.created + "</div>";
 
-                $('.grid').prepend(div_data);
+                $('.grid').append(div_data);
             },
             error: function(jqXHR, status) {
                 console.log(jqXHR);
