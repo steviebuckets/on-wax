531650e611c86ba89b3846a285b9ee41759fc23b
steviebuckets <steviebuckets33@gmail.com>
2017-01-10 12:09:50 -0800

    updated delete button logic

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,35 @@ $(function() {
 
         $.each(data.posts, function(i, data) {
             var div_data =
-                "<div>" + data.title + "<br/>" + data.recordstore + "<br/>" + data.description + "<br/>" + data.user + "<br/> <p class='date'>" + data.created + "<br/>" + '<button type="button" class="btn btn-secondary btn-xs">Delete</button>' + "</div>";
+                "<div>" + data.title + "<br/>" + data.recordstore + "<br/>" + data.description + "<br/>" + data.user + "<br/>" + data.created + "<br/> <p class='tag' data-id=['_id']> + data.id + </p>" +"<br/>" + '<button type="button" class="btn btn-secondary btn-xs">Delete</button>' + "</div>";
 
             var $items = $('<div class="col-md-4"></div');
             $items.append(div_data)
 
             $('.row').append($items);
-                                                                                  
+             $('.btn').click(delete_post);
 
         });
 
+        //deletes post
+        
+           
+   
     });
+
+//delete post function
+function delete_post() {
+    var id = $(this).attr('data-id');
+    jQuery.ajax({
+        url: '/posts',
+        id: this._id,
+        type: 'DELETE',
+        success: function(data) {
+            show_items();
+        }
+    });
+}
+
 
 
     //this creates a new post on submit from user-posts form.
@@ -34,33 +52,7 @@ $(function() {
 
     });
 
-    //this deletes a post
-    function deletePost(data) {
-
-        $.ajax({
-            url: '/posts',
-            dataType: 'json',
-            type: 'POST',
-            id: this._id,
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data),
-            success: function(responseData, status, jqXHR) {
-                console.log(responseData);
-
-                var div_data =
-                    '<div class="col-md-4">' + responseData.title + "<br/> " + responseData.recordstore + "<br/>" + responseData.description + "<br/>" + responseData.user + "<br/>" + responseData.created + "</div>";
-
-                $('.btn').click(function() {
-                    $(this).removeAttr('POST');
-                });
-            },
-            error: function(jqXHR, status) {
-                console.log(jqXHR);
-                alert('fail' + status.code);
-            }
-        });
-    }
-
+   
 
     //sends data from post to server
     function addData(data) {
