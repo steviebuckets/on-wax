29dd379fbd2e2f5da6d643a2cdbd0dfe81f2ad76
steviebuckets <steviebuckets33@gmail.com>
2017-01-24 15:54:50 -0800

    modified config ports

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/on-wax';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 8000;
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
