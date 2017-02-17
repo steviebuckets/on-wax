2618c824ba0ab5ffb8409616165936101e9ab1d9
steviebuckets <steviebuckets33@gmail.com>
2017-02-16 19:28:42 -0800

    changed chai to https

--- config.js
+++ config.js
@@ -1,7 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       'mongodb://sbuckets33:eagleone@ds153699.mlab.com:53699/on-rotation';
-                      /*'mongodb://localhost/on-wax';*/
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
-
-
